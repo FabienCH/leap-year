5f63aa00c0398fc17854e1e506dea7862848052c
FabienCH <fabien.charvaz@gmail.com>
2020-12-19 15:20:01 +0100

    is a leap year if divisible by 400

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -9,4 +9,9 @@ describe('isLeapYear', () => {
     expect(isLeapYear(1996)).toBeTruthy();
     expect(isLeapYear(1992)).toBeTruthy();
   });
+
+  it('a year is a leap year if divisible by 400', () => {
+    expect(isLeapYear(1600)).toBeTruthy();
+    expect(isLeapYear(2000)).toBeTruthy();
+  });
 });
