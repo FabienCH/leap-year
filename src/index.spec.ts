1e89a7a5eb8d0e2829646f3a979d7c7a79cdbb31
FabienCH <fabien.charvaz@gmail.com>
2020-12-19 18:43:51 +0100

    refactor: change describe in tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,21 +1,21 @@
 import { isLeapYear } from './index';
 
-describe('isLeapYear', () => {
-  it('a year is not a leap year if not divisible by 4', () => {
+describe('A year', () => {
+  it('is not a leap year if not divisible by 4', () => {
     expect(isLeapYear(1997)).toBeFalsy();
   });
 
-  it('a year is a leap year if divisible by 4', () => {
+  it('is a leap year if divisible by 4', () => {
     expect(isLeapYear(1996)).toBeTruthy();
     expect(isLeapYear(1992)).toBeTruthy();
   });
 
-  it('a year is a leap year if divisible by 400', () => {
+  it('is a leap year if divisible by 400', () => {
     expect(isLeapYear(1600)).toBeTruthy();
     expect(isLeapYear(2000)).toBeTruthy();
   });
 
-  it('a year is not a leap year if divisible by 100 but not by 400', () => {
+  it('is not a leap year if divisible by 100 but not by 400', () => {
     expect(isLeapYear(1800)).toBeFalsy();
     expect(isLeapYear(2200)).toBeFalsy();
   });
